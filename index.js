3ab55cb86a8d6a3c3f1c21c02cd94bbc0adc5626
Paige <paigehcarroll@gmail.com>
2023-08-31 14:46:58 -0500

    installed express npm package, made db directory with relevant files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const express = require('express');
+
+//initialize instance of express
+const app = express();
+
+//port handler
+const PORT = process.env.PORT || 3001;
+
+//express middleware (urlencoded/url parser, json parser)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
