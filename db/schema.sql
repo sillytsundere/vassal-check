1b202d82eaec971980d7b6cd69dce237bd83c582
Paige <paigehcarroll@gmail.com>
2023-08-31 18:34:53 -0500

    added begining of seed code

--- db/schema.sql
+++ db/schema.sql
@@ -27,7 +27,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id) --whats going on here?--
     ON DELETE SET NULL
