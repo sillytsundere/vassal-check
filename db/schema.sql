806527d290d2ef01c591d10424c5fa0c08edbe4e
Paige <paigehcarroll@gmail.com>
2023-09-01 16:04:58 -0500

    added values to insert commands in seeds file, debugging schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,19 @@
 DROP DATABASE IF EXISTS employees_db;
 
---create employees database--
 CREATE DATABASE employees_db;
 
---select the database for 'use' so all the following code will affect the employees_db
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE position (
     id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary INT NOT NULL,
-    department_id INT NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
@@ -26,9 +23,9 @@ CREATE TABLE employee (
     id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    position_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id) --whats going on here?--
+    FOREIGN KEY (position_id)
+    REFERENCES position(id)
     ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,29 @@
-INSERT INTO department (id)
-VALUES 
+INSERT INTO department (id, name)
+VALUES (5, 'space'), 
+       (3, 'time'), 
+       (1, 'day'), 
+       (2, 'night'), 
+       (7, 'chaos');
+
+INSERT INTO position (id, title, salary, department_id)
+VALUES (77, 'harbringer', 100000, 7),
+       (66, 'omen', 85000, 7),
+       (12, 'clock', 90000, 3),
+       (33, 'sun', 72000, 1),
+       (32, 'moon', 78000, 2),
+       (22, 'electron', 95000, 5),
+       (16, 'matter', 70000, 5),
+       (18, 'dark matter', 63000, 5);
+
+INSERT INTO employee (id, first_name, last_name, position_id, manager_id)
+VALUES (333, 'Iris',	'Cosmos', 77),
+       (112, 'Bartok', 'Chev', 66, 333),
+       (223, 'Felix', 'Raine', 66, 333),
+       (444, 'Chloe', 'Weiss', 12),
+       (355, 'Guy', 'Light', 33, 444),
+       (554, 'Art', 'Simet', 32, 444),
+       (777, 'Shelly', 'Tran', 22),
+       (668, 'Otto', 'Stuff', 16, 777),
+       (996, 'Brock', 'Olle', 18, 777),
+       (228, 'River', 'Still', 18, 777),
+       (633, 'Brea', 'Key', 18, 777);
