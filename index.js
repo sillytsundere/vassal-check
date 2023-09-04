928d109a7202bc1c2f7096cbd7959ceb2aa5107f
Paige <paigehcarroll@gmail.com>
2023-09-04 16:15:40 -0500

    update connection sql

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const express = require('express');
+const express = require("express");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+require('dotenv').config();
 
 //initialize instance of express
 const app = express();
@@ -11,3 +12,69 @@ const PORT = process.env.PORT || 3001;
 //express middleware (urlencoded/url parser, json parser)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+// const db = mysql.createConnection(
+//   {
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: `${process.env.DB_PASSWORD}`,
+//     database: "employees_db",
+//   },
+//   console.log(`Connected to the employees_db database.`)
+// );
+
+function begin() {
+  inquirer
+    .prompt([
+      {
+        name: "task",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((response) => {
+      switch (response.task) {
+        case "View All Departments":
+          console.log("User requested to View All Departments");
+          break;
+        case "View All Roles":
+          console.log("User requested to View All Roles");
+          break;
+        case "View All Employees":
+          console.log("User requested to View All Employees");
+          break;
+        case "Add Department":
+          console.log("User requested to Add a Department");
+          break;
+        case "Add Role":
+          console.log("User requested to Add a Role");
+          break;
+        case "Add Employee":
+          console.log("User requested to Add an Employee");
+          break;
+        case "Update Employee Role":
+          console.log("User requested to Update an Employee Role");
+          break;
+        case "Exit":
+          console.log("Goodbye!");
+          process.exit();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      process.exit();
+    });
+}
+
+begin();
